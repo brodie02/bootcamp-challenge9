100565df1517b6ab05fd05304438d5b0432fd337
brodie02 <marshallbrodie0@gmail.com>
2022-11-02 15:22:28 +1030

    Add questions for user input

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Provide a short description about your project.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the steps required to install your project?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Please provide instructions on how to use your project.',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: '',
+        name: 'contribute',
+    },
+    { // y/n?
+        type: 'input',
+        message: 'Please input any test instructions for your project',
+        name: 'test',
+    },
+    {
+        type: 'list',
+        message: 'Which license does your project use?',
+        name: 'license',
+        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'Mozilla Public License 2.0', 'The Unlicense', 'GNU General Public License v2.0']
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
